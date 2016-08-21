c5187400d427da7266c71e5391d1e3cf26c8c630
Norbert Klose <norbert.klose@web.de>
2016-08-21 02:07:43 +0200

    norbert.klose@web.de

--- sample-cmake.sh
+++ sample-cmake.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
-# usage: sample-cmake.sh [Release|Debug]
+#
+# usage: sample-cmake.sh [Release|Debug] {any other cmake option}*
+#
+# any other cmake option may be
+# -DCMAKE_INSTALL_PREFIX=
+# -DLLDB_API_INCLUDE_DIR=
+# -DLLDB_LIBRARY_DIR=
+
 CMAKE_OPTIONS="-DCMAKE_BUILD_WITH_INSTALL_RPATH=True"
 case "$1" in
     Debug|Release) CONFIG=$1 && shift ;;
                 *) CONFIG=Debug ;;
 esac
+BUILDDIR="build-$CONFIG"
 CMAKE_OPTIONS="${CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=${CONFIG}"
-PREFIX=$HOME/local/llvm-mi
-CMAKE_OPTIONS="${CMAKE_OPTIONS} -DCMAKE_INSTALL_PREFIX=${PREFIX}"
-mkdir build-$CONFIG
-cd build-$CONFIG
-cmake ${CMAKE_OPTIONS} $* ..
+mkdir -p "$BUILDDIR"                             && \
+  cd "$BUILDDIR"                                 && \
+  cmake ${CMAKE_OPTIONS} $* ..                   && \
+  cd ..                                          && \
+  make -C "$BUILDDIR"
